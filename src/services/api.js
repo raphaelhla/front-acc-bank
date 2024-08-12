f95c53eacf4cbebd1bcc8246ece80f5130bc8877
raphaelhla <raphael.agra@ccc.ufcg.edu.br>
2024-08-12 15:39:56 -0300

    fix: coloca url do deploy da api na aws

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://ec2-100-26-107-150.compute-1.amazonaws.com:8080'});
+    baseURL: 'https://api.raphaelagra.com.br'});
 
 export default api;
 
